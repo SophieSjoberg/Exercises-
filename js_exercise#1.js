5781e67f1f889ef829df18389cb818fcc0df16e7
Sophie Sjoberg <sophie.sjoberg@gmail.com>
2017-11-06 13:12:47 +0100

    add js exercise

---
+++ js_exercise#1.js
@@ -0,0 +1,18 @@
+MyArray = [9, 8, 7]
+
+MyArray[1]
+
+var age = 30
+
+if (age < 33 ) {
+alert('still young!')
+      }
+
+
+myArray = ['Sophie', 'Erik', 'Gustaf'];
+
+emptyArray = []
+
+for (i=0; i<myArray.length; i++) {
+emptyArray.push(myArray[i])
+}
